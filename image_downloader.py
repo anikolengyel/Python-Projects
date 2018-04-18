f39a5717002d12edef7ae63791c57b35955717dc
Anikó Lengyel <lengyelaniko93@gmail.com>
2018-04-18 14:30:43 -0700

    Add files via upload
    Modifying the folder paths and names, creating todos.

--- image_downloader.py
+++ image_downloader.py
@@ -4,16 +4,10 @@ import urllib.request
 import os
 from datetime import datetime as dt
 from urllib.parse import urlparse
-
-def writing_html_file():
-    r = requests.get("http://www.origo.hu/index.html")
-    data = r.text
-    print(data)
-    with open('origo_hu_html.html', 'w', encoding="utf-8") as file:
-        file.write(data)
+import sys
 
 def parsing_url(url):
-    netloc = urlparse(url).netloc.split('.')[1]
+    netloc = urlparse(url).netloc.split('.', -1)[-2]
     return netloc
 
 def create_folder():
@@ -24,27 +18,33 @@ def create_folder():
         cwd = os.getcwd()
         print("cwd: ", cwd)
         return cwd
-        #TODO: mi legyen, ha van már folder? (else ág)
+        #TODO: else? if folder already exists
 
-def get_title(pic_url):
-    return (pic_url.split('/', -1))[-1].split('?')[0]
+def get_pic_title(pic_url):
+    title = (pic_url.split('/', -1))[-1].split('?')[0]
+    return title
 
 def download_picture(url):
+    unknown_pictures = []
     try:
         r = requests.get(url)
         data = r.text
         soup = bs(data, "lxml")
-        folder = create_folder() + parsing_url(url)
-        print("folder: ", folder)
+        #todo: folder manipulating function
+        os.makedirs(parsing_url(url)+ "_" + str(dt.now().year)+str(dt.now().month)+ str(dt.now().day))
+        os.chdir(str(os.getcwd()+ "\\" + parsing_url(url)+ "_" + str(dt.now().year)+str(dt.now().month)+ str(dt.now().day)))
+        print(os.getcwd())
         for link in soup.find_all('img'):
+            # extracting the picture link
             source = link.get('src')
-            print(source)
-            fullfilename = os.path.join(folder, get_title(source))
-            print(get_title(source))
-            print("fullfilename: ", fullfilename)
-            urllib.request.urlretrieve(str(source), fullfilename)
-            return url
+            #TODO: source isn't always a in a valid, downloadable url format: Unknown url type
+            # concatenating the folder name and the picture title, creating a path
+            fullfilename = os.path.join(os.getcwd(), get_pic_title(source))
+            urllib.request.urlretrieve(str(source), str(fullfilename))
     except Exception as e:
             print(e)
+            # todo: append the pictures with unknown url to a list, move to the next item
 
-download_picture('http://www.origo.hu/index.html')
+#TODO: no such file or directoty (other links)
+
+download_picture('https://www.nytimes.com/')
