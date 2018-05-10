c9ead1b56db9d8dcfcf33d6ec1d8db2af7ff6bc8
Anikó Lengyel <lengyelaniko93@gmail.com>
2018-05-09 19:22:00 -0700

    flask and web interface

--- image_downloader.py
+++ image_downloader.py
@@ -4,7 +4,10 @@ import urllib.request
 import os
 from datetime import datetime as dt
 from urllib.parse import urlparse
+from flask import Flask, request, render_template
 import sys
+
+app = Flask(__name__, template_folder='static')
 
 def parsing_url(url):
     netloc = urlparse(url).netloc.split('.', -1)[-2]
@@ -45,25 +48,45 @@ def create_list_of_sources(url):
     except Exception as e:
             print(e)
 
-def download_picture(url):
-    #todo: getting the webpage url from input
-    # list to store the invalid picture sources
-    unknown_pictures = []
-    # an integer to count the pictures
-    num = 0
-    folder_path = create_folder(url)
-    try:
-        for source in create_list_of_sources(url):
-            # extracting the picture link
-            #source = link.get('src')
-            num +=1
-            print("Picture number ", num, ", ", source)
-            # concatenating the folder name and the picture title, creating a path to every picture
-            fullfilename = os.path.join(folder_path, get_pic_title(source))
-            print(fullfilename)
-            urllib.request.urlretrieve(str(source), str(fullfilename))
-    except Exception:
-        #catching the unknown urls, appending them to a list
-        unknown_pictures.append(source)
-        pass
-    print(unknown_pictures)
+@app.route('/download_image', methods=['GET', 'POST'])
+def download_picture():
+    # the user starts posting a website:
+    if request.method == 'POST':
+        # if the form is not empty
+        if request.form['url']:
+            # list to store the invalid picture sources
+            unknown_pictures = []
+            # an integer to count the pictures
+            num = 0
+            # getting the url from the form
+            url = request.form['url']
+            print('url: ', url)
+            folder_path = create_folder(url)
+            try:
+                for source in create_list_of_sources(url):
+                    # extracting the picture link
+                    num +=1
+                    print("Picture number ", num, ", ", source)
+                    # concatenating the folder name and the picture title, creating a path to every picture
+                    fullfilename = os.path.join(folder_path, get_pic_title(source))
+                    print(fullfilename)
+                    urllib.request.urlretrieve(str(source), str(fullfilename))
+            except Exception:
+                #catching the unknown urls, appending them to a list
+                unknown_pictures.append(source)
+                pass
+            finally:
+                # change the dir back to the original working dir
+                os.chdir("..")
+            print('Unknown pictures: ', unknown_pictures)
+            return render_template('download_ready.html')
+    else:
+        return render_template('start_page.html')
+
+if __name__ == '__main__':
+    # giving a key to create sessions for users
+    # for development it is just super_secret_key
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
+
